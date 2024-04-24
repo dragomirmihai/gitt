b34767227b99fa280753869661f11dc976588e90
dragomirmihai <dragomir.mihaidanut@yahoo.com>
2024-04-24 15:26:42 +0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+const x = '23';
+if (x === 23) console.log(23);
+
+const calcAge = year => 2037 - year;
+
+console.log('ce faci?');
+console.log('ce faci ionela ?');
+console.log('te iubesc silvia');
+console.log('da');
