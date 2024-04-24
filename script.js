bdafc5fab65cd479235defe835da111bc5607b0f
dragomirmihai <dragomir.mihaidanut@yahoo.com>
2024-04-24 15:53:42 +0300

    Remove Ionela

--- script.js
+++ script.js
@@ -7,6 +7,5 @@ if (x === 23) console.log(23);
 const calcAge = year => 2037 - year;
 
 console.log('ce faci?');
-console.log('ce faci ionela ?');
 console.log('te iubesc silvia');
 console.log('da');
